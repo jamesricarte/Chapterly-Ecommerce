ada41366cdcefc124befde563657ab1d020d6ab6
James Ricarte <126384275+jamesricarte@users.noreply.github.com>
2024-12-30 16:46:54 +0800

    removed product route in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,11 @@ import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import Product from "./pages/Product/Product";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Product />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
