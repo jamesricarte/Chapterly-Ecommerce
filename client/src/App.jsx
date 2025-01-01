510db6e38fa3a12fa1517ce25c7c2e9654291a64
James Ricarte <uhenyou@gmail.com>
2025-01-01 17:51:35 +0800

    initial commit for james branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import Hero from "./pages/Hero/Hero";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Hero />} />
       </Routes>
     </>
   );
