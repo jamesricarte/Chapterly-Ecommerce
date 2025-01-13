5dcd5292f67e6b3a7a09d4ff145d1faca04abfb0
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-01-13 21:42:41 +0800

    Add a slider and remake the navigation dots

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "../../components/Nav";
-import HeroBannerImage from "../../assets/images/backgrounds/Hero_banner.jpg";
+import HeroBannerImage from "../../assets/images/banners/christmas_banner.jpg";
+import YearEndBanner from "../../assets/images/banners/yearEnd_banner.jpg";
+import TopBooksBanner from "../../assets/images/banners/topBooks_banner.jpg";
+import ReadingChallengeBanner from "../../assets/images/banners/readingChallenge_banner.jpg";
 import BookGenresImage1 from "../../assets/images/backgrounds/book_genre_image_1.png";
 import BookGenresImage2 from "../../assets/images/backgrounds/book_genre_image_2.png";
 import BookGenresImage3 from "../../assets/images/backgrounds/book_genre_image_3.png";
@@ -19,6 +22,16 @@ import { HiOutlineChevronRight } from "react-icons/hi";
 import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
 
 const Home = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveSlide((prevSlide) => (prevSlide + 1) % 4);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  console.log(HeroBannerImage);
   return (
     <>
       <Nav />
@@ -30,16 +43,65 @@ const Home = () => {
             src={HeroBannerImage}
             alt="Hero banner"
           />
-          <div className="container px-4 mx-auto">
-            <button className="absolute px-11 py-3 text-2xl font-bold text-white bg-primary-500 rounded-xl bottom-[32%] left-[6.5%] hover-preset-1">
-              Shop Now
-            </button>
-            <div className="absolute flex gap-3 transform -translate-x-1/2 bottom-6 left-1/2">
-              <div className="w-4 h-4 border border-black rounded-full bg-secondary-500"></div>
-              <div className="w-4 h-4 bg-white border border-black rounded-full"></div>
-              <div className="w-4 h-4 bg-white border border-black rounded-full"></div>
-              <div className="w-4 h-4 bg-white border border-black rounded-full"></div>
+          <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+            <div
+              className="flex transition-transform duration-500"
+              style={{ transform: `translateX(-${activeSlide * 100}%)` }}
+            >
+              {/* Slide 1 */}
+              <div className="relative flex-shrink-0 w-full h-full">
+                <img
+                  className="object-cover w-full h-full"
+                  src={HeroBannerImage}
+                  alt="Slide 1"
+                />
+                <button className="absolute px-11 py-3 text-2xl font-bold text-white bg-primary-500 rounded-xl bottom-[32%] left-[6.5%] hover-preset-1">
+                  Shop Now
+                </button>
+              </div>
+              {/* Slide 2 */}
+              <div className="relative flex-shrink-0 w-full h-full">
+                <img
+                  className="object-cover w-full h-full"
+                  src={YearEndBanner}
+                  alt="Slide 2"
+                />
+                <button className="absolute p-7 text-3xl font-bold text-white bg-[#3ccabc] w-[390px]  rounded-full bottom-[26.5%] left-[57%] transition-all duration-300 ease-in-out transform  hover:opacity-80">
+                  Shop Now
+                </button>
+              </div>
+              {/* Slide 3 */}
+              <div className="relative flex-shrink-0 w-full h-full">
+                <img
+                  className="object-cover w-full h-full"
+                  src={TopBooksBanner}
+                  alt="Slide 3"
+                />
+                <button className="absolute p-7 text-3xl font-bold text-[#26625B] bg-[#00a59a] w-[440px] rounded-xl bottom-[29.5%] left-[61.3%] transition-all duration-300 ease-in-out transform  hover:opacity-90">
+                  READ MORE
+                </button>
+              </div>
+              {/* Slide 4 */}
+              <div className="relative flex-shrink-0 w-full h-full">
+                <img
+                  className="object-cover w-full h-full"
+                  src={ReadingChallengeBanner}
+                  alt="Slide 4"
+                />
+              </div>
             </div>
+          </div>
+
+          <div className="absolute flex gap-5 px-5 py-3 transform -translate-x-1/2 bg-white rounded-full bottom-6 left-1/2">
+            {[0, 1, 2, 3].map((index) => (
+              <div
+                key={index}
+                onClick={() => setActiveSlide(index)} // Click to change slide
+                className={`w-6 h-6 rounded-full border border-white cursor-pointer ${
+                  activeSlide === index ? "bg-primary-500" : "bg-secondary-200"
+                }`}
+              ></div>
+            ))}
           </div>
         </section>
 
@@ -70,11 +132,11 @@ const Home = () => {
                 <button className="absolute px-11 py-3 text-xs font-bold text-white bg-primary-500 rounded-xl bottom-[26%] left-[14%] hover-preset-1">
                   Buy now
                 </button>
-                <div className="absolute flex gap-3 transform -translate-x-1/2 bottom-12 left-1/2">
-                  <div className="w-5 h-5 border border-black rounded-full bg-secondary-500"></div>
-                  <div className="w-5 h-5 bg-white border border-black rounded-full"></div>
-                  <div className="w-5 h-5 bg-white border border-black rounded-full"></div>
-                  <div className="w-5 h-5 bg-white border border-black rounded-full"></div>
+                <div className="absolute flex gap-3 px-5 py-3 transform -translate-x-1/2 bg-white rounded-full bottom-12 left-1/2">
+                  <div className="w-5 h-5 border border-black rounded-full bg-primary-500"></div>
+                  <div className="w-5 h-5 rounded-full bg-secondary-200"></div>
+                  <div className="w-5 h-5 rounded-full bg-secondary-200"></div>
+                  <div className="w-5 h-5 rounded-full bg-secondary-200"></div>
                 </div>
               </div>
 
